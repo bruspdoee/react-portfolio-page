9fc6ae0915d0033a9f3b154ad97cec6ac15657e5
Brus Palaj <brus.palaj71@gmail.com>
2020-06-18 20:55:11 -0400

    added contact info on contact page, needs some styling work but will do for now

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
+import { Grid, Cell, ListItem, List, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1>Contact Page</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={12}>
+            <h2>Contact Me</h2>
+            <hr />
+            <List>
+              <ListItem>
+                <ListItemContent style={{ fontSize: "30px" }}>
+                  <i className="fa fa-phone-square" />
+                  (929) 389-4965
+                </ListItemContent>
+              </ListItem>
+              <ListItem>
+                <ListItemContent style={{ fontSize: "30px" }}>
+                  <i className="fa fa-envelope" /> brus.palaj71@gmail.com
+                </ListItemContent>
+              </ListItem>
+            </List>
+          </Cell>
+        </Grid>
       </div>
     );
   }
